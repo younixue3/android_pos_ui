8e5febb5dab6ba4bad2959e6716f6ef360bf3de1
Ricko Caesar Aprilla Tiaka <bussiness.needs.rctka@gmail.com>
2021-10-18 22:36:04 +0800

    New Layout Button bar

--- lib/main.dart
+++ lib/main.dart
@@ -34,59 +34,147 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      children: [
-        Container(
-          height: 220,
-          decoration: BoxDecoration(color: Colors.indigo),
-        ),
-        SafeArea(
-          child: Container(
+      body: Stack(
+        children: [
+          Container(
+            height: 220,
+            decoration: BoxDecoration(color: Colors.indigo),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 20, left: 10),
+            margin: EdgeInsets.only(top: 20),
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 IconButton(
+                  onPressed: () {},
                   icon: Icon(Icons.menu, color: Colors.white),
-                  onPressed: null,
                 ),
-                IconButton(
-                  icon: Icon(Icons.menu, color: Colors.white),
-                  onPressed: null,
-                ),
+                Container(
+                  height: 40,
+                  width: 40,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage(
+                              'assets/public/57774948_2440392989306240_790466375524024320_n.jpg'))),
+                )
               ],
             ),
           ),
-        ),
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 20),
-          margin: EdgeInsets.only(top: 70),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Hi, Ricko Tiaka",
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w300,
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            margin: EdgeInsets.only(top: 70),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Hi, Ricko Tiaka",
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w300,
+                  ),
+                  textAlign: TextAlign.right,
                 ),
-                textAlign: TextAlign.right,
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 10),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 180,
-                  child: Card(
-                    color: Color.fromRGBO(234, 234, 234, 1),
-                    elevation: 4,
-                    child: LineChartHome(),
+                Container(
+                  margin: EdgeInsets.only(top: 90),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Card(
+                        color: Colors.indigoAccent,
+                        child: Container(
+                          padding: EdgeInsets.symmetric(horizontal: 10),
+                          child: Wrap(
+                            spacing: 15,
+                            children: [
+                              IconButton(
+                                splashColor: Colors.indigo,
+                                splashRadius: 32,
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.stacked_line_chart,
+                                  color: Colors.white,
+                                  size: 28,
+                                ),
+                              ),
+                              IconButton(
+                                splashColor: Colors.indigo,
+                                splashRadius: 32,
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.menu_book,
+                                  color: Colors.white38,
+                                  size: 28,
+                                ),
+                              ),
+                              IconButton(
+                                splashColor: Colors.indigo,
+                                splashRadius: 32,
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.food_bank,
+                                  color: Colors.white38,
+                                  size: 28,
+                                ),
+                              ),
+                              IconButton(
+                                splashColor: Colors.indigo,
+                                splashRadius: 32,
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.stacked_line_chart,
+                                  color: Colors.white38,
+                                  size: 28,
+                                ),
+                              ),
+                              IconButton(
+                                splashColor: Colors.indigo,
+                                splashRadius: 32,
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.stacked_line_chart,
+                                  color: Colors.white38,
+                                  size: 28,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                          child: GridView.count(
+                        crossAxisCount: 2,
+                        children: [
+                          Container(
+                            child: Card(
+                              child: Text('data'),
+                            ),
+                          )
+                        ],
+                      ))
+                    ],
                   ),
                 ),
-              )
-            ],
+                // Container(
+                //   margin: EdgeInsets.only(top: 10),
+                //   child: SizedBox(
+                //     width: double.infinity,
+                //     height: 180,
+                //     child: Card(
+                //       color: Color.fromRGBO(234, 234, 234, 1),
+                //       elevation: 4,
+                //       child: LineChartHome(),
+                //     ),
+                //   ),
+                // )
+              ],
+            ),
           ),
-        ),
-      ],
-    ));
+        ],
+      ),
+    );
   }
 }
